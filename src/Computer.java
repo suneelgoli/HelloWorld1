daf5137488b6397c542018d1330e2eb453d15c61
Suneel Kumar Goli <suneelgoli@gmail.com>
2018-12-26 21:11:13 +0530

    this is first commit

---
+++ src/Computer.java
@@ -0,0 +1,8 @@
+public class Computer {
+    public static void main(String []args)
+    {
+       Long input=10l;
+       System.out.println(Long.rotateLeft(input,2));
+       System.out.println(Long.signum(-20));
+    }
+}
